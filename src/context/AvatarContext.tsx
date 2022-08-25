6fc8f1944591d26b4133da74f46c40a63432d650
Gabriel Freitas <gabriel.freitas.santos@g.globo>
2022-08-25 16:33:20 -0300

    added context api

---
+++ src/context/AvatarContext.tsx
@@ -0,0 +1,49 @@
+import { createContext, ReactNode, useCallback, useState, useMemo, useEffect } from 'react';
+
+interface AvatarProviderProps {
+   children: ReactNode;
+}
+
+interface AvatarContextData {
+   avatar: File | string;
+   uploadAvatar: (file: File | string) => void;
+   canvaAvatar: string;
+   setCanvaAvatar: (value: string) => void;
+   template: 'initial' | 'error' | 'cropping' | 'cropped';
+   setTemplate: (tmp: 'initial' | 'error' | 'cropping' | 'cropped') => void;
+   avatarScale: number;
+   setAvatarScale: (sacel: number) => void;
+   loadFileIsEnabled: boolean;
+   setLoadFileIsEnabled: (value: boolean) => void;
+}
+
+export const AvatarContext = createContext({} as AvatarContextData);
+
+export const AvatarProvider = ({ children }: AvatarProviderProps) => {
+   const [avatar, setAvatar] = useState<File | string>({} as File);
+   const [canvaAvatar, setCanvaAvatar] = useState<string>('');
+   const [avatarScale, setAvatarScale] = useState<number>(1);
+   const [template, setTemplate] = useState<'initial' | 'error' | 'cropping' | 'cropped'>('initial');
+   const [loadFileIsEnabled, setLoadFileIsEnabled] = useState<boolean>(true);
+
+   const uploadAvatar = (file: File | string) => {
+      setAvatar(file);
+   };
+
+   const providerValue = useMemo(() => {
+      return {
+         avatar,
+         uploadAvatar,
+         template,
+         setTemplate,
+         avatarScale,
+         setAvatarScale,
+         canvaAvatar,
+         setCanvaAvatar,
+         loadFileIsEnabled,
+         setLoadFileIsEnabled,
+      };
+   }, [avatar, setAvatar, template, setTemplate, avatarScale, canvaAvatar]);
+
+   return <AvatarContext.Provider value={providerValue}>{children}</AvatarContext.Provider>;
+};
